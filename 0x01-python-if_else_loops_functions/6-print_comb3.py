203a2d6ae00ac918a0870c6c264ad3b575f4277c
Devix-old <mohamed.elgharef@gmail.com>
2023-08-08 15:57:19 +0100

    from task 1 to 5 first attempt

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(1,89):
+for i in range(1, 89):
     if i < 10:
-        print('0',end="")
-    if i / 10 < i % 10 :
-        print("{} ,".format(i),end="")
+        print('0', end="")
+    if i / 10 < i % 10:
+        print("{} ,".format(i), end="")
 print("89")
