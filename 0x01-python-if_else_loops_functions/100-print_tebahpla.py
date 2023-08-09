ae189f4f52c594af64100b8d188301b87594ed6b
Devix-old <mohamed.elgharef@gmail.com>
2023-08-09 15:17:26 +0100

    task 14 pycodestyle corrected

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,11 +2,11 @@
 alpha = 122
 i = 0
 start = 1
-while(i <= 25):
+while i <= 25:
     if start % 2 == 0:
-        print("{}".format(chr(alpha - 32)),end="")
+        print("{}".format(chr(alpha - 32)), end="")
     else:
-        print("{}".format(chr(alpha)),end="")
+        print("{}".format(chr(alpha)), end="")
     alpha = alpha - 1
     i = i + 1
     start += 1
