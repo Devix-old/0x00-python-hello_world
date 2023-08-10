73245cd54994c0856a9991c67cf5d26669de9db1
Devix-old <mohamed.elgharef@gmail.com>
2023-08-10 19:51:16 +0100

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,17 +5,16 @@ if __name__ == "__main__":
     len_argv = len(sys.argv) - 1
     if len_argv != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
-    match operator:
-        case '-':
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        case '+':
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        case '*':
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        case '/':
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    if operator == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif operator == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif operator == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif operator == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
