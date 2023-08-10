8608f171db32e8ac1c89629e15999840d0e23e5e
Devix-old <mohamed.elgharef@gmail.com>
2023-08-10 16:48:26 +0100

    Pycodestyle fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments".format(len(sys.argv) - 1),end="")
+    print("{} arguments".format(len(sys.argv) - 1), end="")
     if len(sys.argv) - 1 > 0:
         print(":")
     elif len(sys.argv) - 1 == 0:
-        print(".")	
+        print(".")
     for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))	
+        print("{}: {}".format(i, sys.argv[i]))
