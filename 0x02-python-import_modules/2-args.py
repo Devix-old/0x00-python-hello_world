48c0f418c4aa06cf10ff08faa86cc80b15e612c7
Devix-old <mohamed.elgharef@gmail.com>
2023-08-10 16:45:02 +0100

    I add Some correction to task 2 (print . if no arguments passed and : if there is arguments)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments.".format(len(sys.argv) - 1))
+    print("{} arguments".format(len(sys.argv) - 1),end="")
+    if len(sys.argv) - 1 > 0:
+        print(":")
+    elif len(sys.argv) - 1 == 0:
+        print(".")	
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))	
