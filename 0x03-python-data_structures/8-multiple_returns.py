477b960620c9452c75b499a63397597b54a4a1ef
Devix-old <mohamed.elgharef@gmail.com>
2023-08-12 18:36:31 +0100

    sss

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+    Function that returns a tuple with the length
+    of a string and its first character.
+    ...
+
+    Parameters
+    ----------
+    sentence : str
+        The string to check
+
+    Return:
+        tuple
+    """
 def multiple_returns(sentence):
     if sentence == None:
         return 0, None
