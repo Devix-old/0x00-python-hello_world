24146fd57985e6dcf11b310d26b8770e1841c0aa
Devix-old <mohamed.elgharef@gmail.com>
2023-08-16 21:00:56 +0100

    More corrections

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_list = []
-    for i in set_1:
-        if i not in set_2:
-            new_list.append(i)
-    for y in set_2:
-        if y not in set_1:
-            new_list.append(y)
-    return new_list
+    return set_1 ^ set_2
