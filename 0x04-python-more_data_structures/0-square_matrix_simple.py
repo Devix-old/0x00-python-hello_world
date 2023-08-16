8cb78ce1e91be616ad04333013591a4117bdd06f
Devix-old <mohamed.elgharef@gmail.com>
2023-08-16 20:42:39 +0100

    First commit 10 Tasks

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        squared_row = []
-        for num in row:
-            squared_row.append(num * num)
-        new_matrix.append(squared_row)
-    return(new_matrix)
+    return ([list(map(lambda num: num * num, row)) for row in matrix])
