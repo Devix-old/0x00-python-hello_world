3de24400d12178e56cc2e64df342637210143dbb
Devix-old <mohamed.elgharef@gmail.com>
2023-08-28 21:32:09 +0100

    task 100 commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (ValueError, TypeError) as erreur:
+        print("Exception: {}".format(erreur), file=sys.stderr)
+        return (False)
