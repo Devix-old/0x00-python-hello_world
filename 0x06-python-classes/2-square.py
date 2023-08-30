b8293383e341db866d071c64a62f44068d6c9fea
Devix-old <mohamed.elgharef@gmail.com>
2023-08-30 02:57:01 +0100

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""class that define a square"""
+
+
+class Square:
+    """class Square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+        Args:
+        size (int) : The size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+            
+
