98d3f2504750e88c6057dff7590fd32a190428e6
Devix-old <mohamed.elgharef@gmail.com>
2023-08-30 03:02:49 +0100

    Task 2 Correction

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""class that define a square"""
+
+
+class Square:
+    """class Square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+        Args:
+        size (int) : The size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        return (self.__size ** 2)
