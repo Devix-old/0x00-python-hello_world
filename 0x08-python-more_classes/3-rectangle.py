d2d99b1876a1d903e5f63ddde6354fbc22106e14
Devix-old <mohamed.elgharef@gmail.com>
2023-09-05 00:16:00 +0100

    First attempt

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+This module defines the Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new Rectangle instance.
+
+        Args:
+            width (int, optional): The width of the rectangle (default is 0).
+            height (int, optional): The height of the rectangle (default is 0).
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Getter method for the width attribute.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for the width attribute.
+
+        Args:
+            value (int): The new width value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle."""
+        if self.height == 0 or self.width == 0:
+            return 0
+        return (self.height + self.width) * 2
+
+    def __str__(self):
+        if self.height == 0 or self.width == 0:
+            return ""
+        return (("#" * (self.width) + "\n") * self.height)
