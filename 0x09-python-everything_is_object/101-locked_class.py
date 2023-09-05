ab598d5524f9e2e0ac12c24a42295b2168059acd
Devix-old <mohamed.elgharef@gmail.com>
2023-09-05 22:50:46 +0100

    Commiting advanced tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Define locked_class module"""
+
+
 class LockedClass:
     """Representation of the class LockedClass"""
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            raise AttributeError(
-                "'LockedClass' object has no attribute '{}'".format(name))
+    __slots__ = ['first_name']
