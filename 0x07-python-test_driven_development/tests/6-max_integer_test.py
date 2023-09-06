cbbce8f323092d3a34b540cde38dafa4e6582a2d
Devix-old <mohamed.elgharef@gmail.com>
2023-09-06 19:45:16 +0100

    python test driven dev

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,21 +5,6 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
-
-
 class test_max_integer(unittest.TestCase):
     def test_max_int(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
