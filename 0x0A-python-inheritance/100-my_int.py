3d6d0d67456b8d354caf252f10bbbe9fe5f257fa
Devix-old <mohamed.elgharef@gmail.com>
2023-09-11 22:49:29 +0100

    sss

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module defines the MyInt class, which is a custom integer class that inverts the behavior
+of the == and != operators when comparing MyInt objects with other objects.
+"""
+
+
+class MyInt(int):
+    """
+    MyInt is a custom integer class that inherits from the built-in int class.
+    """
+    def __eq__(self, other):
+        """
+        Override the == operator to invert its behavior.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Override the != operator to invert its behavior.
+
+        Args:
+            other: The object to compare with MyInt.
+
+        Returns:
+            bool: True if the objects are equal, False if they are not equal.
+        """
+        return super().__eq__(other)
