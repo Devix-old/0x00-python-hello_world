699a071f1980279a989990928e34d3c4fda4c3bc
Devix-old <mohamed.elgharef@gmail.com>
2023-10-18 03:22:55 +0100

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Convert the 'first_table' to utf8mb4_unicode_ci
+-- Convert the database to utf8mb4_unicode_ci
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
