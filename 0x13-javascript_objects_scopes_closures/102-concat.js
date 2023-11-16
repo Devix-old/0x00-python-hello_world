bd005a460a7a979fb6a5e0f0f9926e59395de26b
Devix-old <mohamed.elgharef@gmail.com>
2023-11-16 03:50:28 +0100

    Advanced tasks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const fs = require('fs');
+
+fs.readFile(args[0], 'utf8', (err1, data1) => {
+  if (err1) {
+    return;
+  }
+
+  fs.readFile(args[1], 'utf8', (err2, data2) => {
+    if (err2) {
+      return;
+    }
+
+    const content = data1 + data2;
+    fs.writeFile(args[2], content, 'utf8', (err3) => {
+    });
+  });
+});
