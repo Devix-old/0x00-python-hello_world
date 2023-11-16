d4a9cb7ee411d6a0c509297377dc491b7a3865d3
Devix-old <mohamed.elgharef@gmail.com>
2023-11-16 03:55:30 +0100

    task 100 correction

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const map1 = list.map((x) => x * 2);
+const map1 = list.map((value, index) => value * index);
 console.log(list);
 console.log(map1);
