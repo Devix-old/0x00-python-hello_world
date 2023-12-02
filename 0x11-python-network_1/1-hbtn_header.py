661c2a0f98262a66962ffa316642cbabf08fcd8e
Devix-old <mohamed.elgharef@gmail.com>
2023-12-02 17:46:56 +0100

    git add .!

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Displays the value of the X-Request-Id variable
+found in the header of the response."""
 import urllib.request
 import sys
-
-with urllib.request.urlopen(sys.argv[1]) as html:
-    if "X-Request-Id" in html.headers:
-    	print(html.headers["X-Request-Id"])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        if 'X-Request-Id' in response.headers:
+            print(response.headers['X-Request-Id'])
