aa0a252d037ad3e42a273c3f36cb1f6348771308
Devix-old <mohamed.elgharef@gmail.com>
2023-12-04 16:14:08 +0100

    git add .!

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,17 @@
-#!/usr/bin/python3
-"""sends a POST request to the passed URL with the email as a parameter"""
 import urllib.parse
 import urllib.request
 import sys
+
 if __name__ == "__main__":
-    
+    # Extract URL and email from command line arguments
     url = sys.argv[1]
-    data = {"email": sys.argv[2]}
-    encoded_data = urllib.parse.urlencode(data).encode("utf-8")
+    email = sys.argv[2]
+
+    # Prepare data for the POST request
+    data = {'email': email}
+    encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+
+    # Send the POST request and display the response
     with urllib.request.urlopen(url, data=encoded_data) as response:
-        print(response.read().decode("utf-8"))
-    
+        response_body = response.read().decode('utf-8')
+        print(response_body)
