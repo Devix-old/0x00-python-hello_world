975c68430a0b74bdaf3ce28c955eadbd99a375b6
Devix-old <mohamed.elgharef@gmail.com>
2023-12-04 17:03:06 +0100

    clear!

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import requests
-import sys
 """ Python script that takes in a URL, sends a request to the URL and displays
 the body of the response."""
+
+import requests
+import sys
+
 if __name__ == "__main__":
     url = sys.argv[1]
 
