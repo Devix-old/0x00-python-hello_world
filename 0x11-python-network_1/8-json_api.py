08175a6c34b0bc51e351df7d1aecce7649259b68
Devix-old <mohamed.elgharef@gmail.com>
2023-12-04 17:02:21 +0100

    clear!

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     payload = {'q': letter}
 
     try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+        response = requests.post(
+            "http://0.0.0.0:5000/search_user", data=payload)
         json_response = response.json()
 
         if json_response:
