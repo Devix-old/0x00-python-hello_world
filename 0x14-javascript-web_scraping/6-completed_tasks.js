000cdc1b659245d9f2632aa7671d37f66d516233
Devix-old <mohamed.elgharef@gmail.com>
2024-01-31 18:45:26 +0100

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, 'utf-8', (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Invalid response:', response.statusCode);
+    return;
+  }
+  const dict = {};
+  for (const task of JSON.parse(body)) {
+    dict[task.userId] = 0;
+  }
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      dict[task.userId]++;
+    }
+  }
+  console.log(dict);
+});
